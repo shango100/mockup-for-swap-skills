f403c840c98a22b163034fdd711fabd52e003bba
shango100 <83172130+shango100@users.noreply.github.com>
2025-03-04 15:52:06 +0100

    initialization  of project
    initial upload of main.html , styles.css and script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the splash screen animation to finish
+setTimeout(() => {
+  // Redirect to the main app screen or perform other actions
+  window.location.href = "main.html"; // Replace with your main screen URL
+}, 4000); // Adjust timing to match animation duration
